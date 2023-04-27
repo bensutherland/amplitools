36f9ccce1c3991e79b0725b3a6fb99ffde494f12
bensutherland <bensutherland7@gmail.com>
2023-04-27 10:12:30 -0700

    Complete functionalization (early version)

--- 01_scripts/proton_to_genepop.R
+++ 01_scripts/proton_to_genepop.R
@@ -8,13 +8,13 @@ proton_to_genepop <- function(hotspot_only = TRUE, neg_control="BLANK"){
   # Set nulls
   input.df <- NULL
   
-  for(i in 1:length(inputs)){
+  for(c in 1:length(inputs)){
     
     
     #### 01. Read in input, create run.name__barcode__sample.name identifier, then reduce to essential columns ####
     # Read in input
-    print(paste0("Reading in file ", inputs[i]))
-    input.df <- read.delim(file = paste0("02_input_data/", inputs[i]), header = T, sep = "\t")
+    print(paste0("Reading in file ", inputs[c]))
+    input.df <- read.delim(file = paste0("02_input_data/", inputs[c]), header = T, sep = "\t")
     print("Data loaded; rows, cols:")
     print(dim(input.df))
     
@@ -31,8 +31,6 @@ proton_to_genepop <- function(hotspot_only = TRUE, neg_control="BLANK"){
     
     # Reporting
     print(paste0("Currently, there are ", length(unique(input.df$Allele.Name)), " unique markers"))
-    num.markers <- length(unique(input.df$Allele.Name))
-    
     
     # Remove non-hotspot if required
     if(hotspot_only==TRUE){
@@ -123,12 +121,6 @@ proton_to_genepop <- function(hotspot_only = TRUE, neg_control="BLANK"){
     # Now have a complete, allele-based matrix
     head(input.df, n = 10)
     
-    # # Save output
-    # write.table(x = proton_trim.df, file = paste0("03_results/", proton.FN, "_proton_data_converted.txt")
-    #             , quote = F, sep = "\t", row.names = F
-    # )
-     
-    
     # Removing negative control samples
     # What are the neg. control samples?
     print(paste0("Removing, ", length(unique(input.df[grep(pattern = neg_control, x = input.df$identifier), "identifier"]))
@@ -146,98 +138,126 @@ proton_to_genepop <- function(hotspot_only = TRUE, neg_control="BLANK"){
     print(table(input.df$genepop))
     print(paste0("Total number of genotypes in formatted output: ", sum(table(input.df$genepop))))
     
+    ### TODO: output this as a data output (nucleotides lost below)
+    
+    # # Save output
+    # write.table(x = proton_trim.df, file = paste0("03_results/", proton.FN, "_proton_data_converted.txt")
+    #             , quote = F, sep = "\t", row.names = F
+    # )
     
     
+    #### 03. Restructure to create genotype matrix for genepop format  ####
+    ## Data check
+    if(length(table(table(input.df$identifier)))==1){
+      
+      print("All individuals have an equal number of rows (markers), as required, in total per indiv: ")
+      print(length(unique(input.df$Allele.Name)))
+      
+    }else{
+      
+      print("Warning! Not all individuals have the equal number of rows, and so something has gone wrong. Need to debug.")
+      
+    }
+      
     
+    # Reporting
+    print("Building genotype matrix (df) to be used for genepop construction")
+    
+    # Identify all samples present
+    samples <- unique(input.df$identifier)
+    
+    ## Per sample, collect the marker names and genepop-formatted genotypes
+    # Set nulls
+    soi <- NULL; line.item <- NULL; genetics.df <- NULL; line.item.df <- NULL
+    # Loop
+    for(j in 1:length(samples)){
+      
+      # Identify the sample name for this round
+      soi <- samples[j]
+      
+      ## Reporting (debugging)
+      # print(paste0("j = ", j))
+      # print(soi)
+      
+      # For this sample, retain all marker names and corresponding genepop genotypes
+      line.item <- input.df[input.df$identifier==soi, c("Allele.Name", "genepop")]
+      
+      # Set the genepop genotype column name as the sample name
+      colnames(x = line.item)[which(colnames(line.item)=="genepop")] <- soi
+      
+      # Convert to df
+      line.item.df <- as.data.frame(line.item)
+      rm(line.item)
+      
+      # Build the full genotype df
+      # If this is the first record, initialize the df
+      if(j==1){
+        
+        genetics.df <- line.item.df
+        
+        # If it is a subsequent record, merge with the existing df by marker names
+      }else if(j>1){
+        
+        genetics.df <- merge(x = genetics.df, y = line.item.df, by = "Allele.Name", all.x = T)
+        # Note: the all.x is necessary here to avoid issues if the number of markers is not identical
+        
+      }
+      
+      #print(head(genetics.df))
+      
+    }
+    
+    # Observe output
+    dim(genetics.df)
+    genetics.df[1:4, 1:4]
+    
+    # # Save output
+    # write.table(x = genetics.df, file = paste0("03_results/", proton.FN, "_genetic_data_only.txt"), quote = F, sep = "\t", row.names = F)
+    
+    # Reshape the output; transpose to make samples as rows and markers as columns
+    genetics_prep.df <- t(genetics.df)
+    genetics_prep.df[1:4, 1:4]
+    print(dim(genetics_prep.df))
+    rm(genetics.df)
+    
+    # Include the rownames (indiv names) as a new column
+    genotypes.df <- cbind(rownames(genetics_prep.df), genetics_prep.df)
+    genotypes.df[1:3,1:3]
+    rm(genetics_prep.df)
+    
+    # Use the marker names (the first row) as colnames for the object
+    colnames(x = genotypes.df) <- genotypes.df[1,]
+    genotypes.df[1:5,1:5]
+    
+    # Rename the column 'Allele.Name' as the more correct, 'indiv' 
+    colnames(genotypes.df)[which(colnames(genotypes.df)=="Allele.Name")] <- "indiv"
+    genotypes.df[1:3,1:3]
+    
+    # Remove the row containing the old header information
+    genotypes.df <- genotypes.df[grep(pattern = "Allele.Name", x = genotypes.df[,"indiv"], invert = T), ]
+    
+    # View
+    genotypes.df[1:5,1:5]
+    
+    # Reporting
+    print("The genotype matrix is now prepared.")
+    
+    # Create output filename
+    output.FN <- paste0("02_input_data/prepped_matrices/"
+                        , gsub(pattern = "\\.xls", replacement = "", x = inputs[c])
+                        , "_gen_data.txt"
+                        )
+    
+    # Reporting
+    print(paste0("Saving output to ", output.FN))
+    
+    # Save output
+    write.table(x = genotypes.df, file = output.FN, quote = F, sep = "\t", row.names = F)
     
   }
   
 }
 
 
-
-
-
-#### 03. Change structure into matrix to prepare for genepop format ####
-# Confirm that all of the samples have an equal number of records
-table(table(proton_trim.df$identifier)==num.markers)
-
-# Identify all samples present
-samples <- unique(proton_trim.df$identifier)
-
-# Per sample, loop to collect the marker names and genepop genotypes, now labeled with identifier
-# Set nulls
-soi <- NULL; line.item <- NULL; genetics.df <- NULL; line.item.df <- NULL
-
-for(i in 1:length(samples)){
-  
-  # Identify the sample name for this round
-  soi <- samples[i]
-  
-  # Reporting
-  print(paste0("i = ", i))
-  print(soi)
-  
-  # For this sample, retain all marker names and corresponding genepop genotypes
-  line.item <- proton_trim.df[proton_trim.df$identifier==soi, c("Allele.Name", "genepop")]
-  
-  # For this record, set the genepop column name as the sample name
-  colnames(x = line.item)[which(colnames(line.item)=="genepop")] <- soi
-  
-  # Convert record to df
-  line.item.df <- as.data.frame(line.item)
-  
-  # Build a full df using the record
-  # If it is the first record, initialize the df
-  if(i==1){
-    
-    genetics.df <- line.item.df
-    
-  # If it is a subsequent record, merge with the existing df by marker names
-  }else if(i>1){
-    
-    genetics.df <- merge(x = genetics.df, y = line.item.df, by = "Allele.Name", all.x = T)
-    # Note: the all.x is necessary here to avoid issues if the number of markers is not identical
-    
-  }
-  
-  #print(head(genetics.df))
-  
-}
-
-# Observe output
-dim(genetics.df)
-genetics.df[1:4, 1:4]
-
-# Save output
-write.table(x = genetics.df, file = paste0("03_results/", proton.FN, "_genetic_data_only.txt"), quote = F, sep = "\t", row.names = F)
-
-# Reshape the output; transpose to columns as markers
-genetics_prep.df <- t(genetics.df)
-genetics_prep.df[1:4, 1:4]
-dim(genetics_prep.df)
-
-
-# Include the rownames (indiv names) as a new column
-genotypes.df <- cbind(rownames(genetics_prep.df), genetics_prep.df)
-genotypes.df[1:3,1:3]
-
-# Use the marker names (the first row) as colnames for the object
-colnames(x = genotypes.df) <- genotypes.df[1,]
-genotypes.df[1:5,1:5]
-
-# Rename the column 'Allele.Name' as the more correct, 'indiv' 
-colnames(genotypes.df)[which(colnames(genotypes.df)=="Allele.Name")] <- "indiv"
-genotypes.df[1:3,1:3]
-
-# Remove the row containing the old header information (used to contain markers)
-genotypes.df <- genotypes.df[grep(pattern = "Allele.Name", x = genotypes.df[,"indiv"], invert = T), ]
-
-# View
-genotypes.df[1:5,1:5]
-
-# Save output
-paste0("03_results/", proton.FN, "_genetic_data_only.txt")
-write.table(x = genotypes.df, file = paste0("03_results/", proton.FN, "_genetic_data_only_final.txt"), quote = F, sep = "\t", row.names = F)
 
 # See README for directions on completing the genepop in shell
