c9f5987b1595fc89dd6594d41f1393705441630d
bensutherland <bensutherland7@gmail.com>
2023-06-09 06:48:50 -0700

    Provide updated instructions in demo script

--- 01_scripts/demo_analysis.R
+++ 01_scripts/demo_analysis.R
@@ -15,18 +15,32 @@ comp_tech_reps(format_type = "amplitools", max_missing = 0.5)
 
 save(obj_nr_best, file="02_input_data/obj_nr_best_2023-05-01.RData")
 
-# Restart? 
+# Restart before popgen analysis but after comparing tech reps? 
+# Clear workspace and source simple_pop_stats_start.R
 load("02_input_data/obj_nr_best_2023-05-01.RData")
 
-# Next use 
-obj_nr_best
-# for all downstream analysis
+# Next use for all downstream popgen analyses
+obj_nr_best 
+pop(obj_nr_best) # note that all pop attributes are 'unkn' currently, they will be added in the script below
 
 # Complete all described in 'ms_amplicon_panel/01_scripts/sps_popgen_analysis.R'
+# This will output a rubias formatted file to 'amplitools/03_prepped_data/cgig_all_rubias.txt'
 
 # Source 00_initiator.R
 
 # Estimate log likelihoods from the data and simulated sibs/ parents, then calculate on your existing data
-# provide logl cutoff
-ckmr_from_rubias(input.FN = "03_prepped_data/cgig_all_rubias.txt", parent_pop = "VIU_F1", offspring_pop = "VIU_F2", parent_pattern = "BR", cutoff = 5)
+# Running manually
+input.FN = "03_prepped_data/cgig_all_rubias.txt"
+cutoff = 5
+parent_pop = "VIU_F1"
+offspring_pop = "VIU_F2"
+#OR
+parent_pop = "VIU_F0"
+offspring_pop = "VIU_F1"
+# then run ckmr_from_rubias.R interactively
 
+
+# provide logl cutoff
+ckmr_from_rubias(input.FN = "03_prepped_data/cgig_all_rubias.txt", parent_pop = "VIU_F1", offspring_pop = "VIU_F2", cutoff = 5)
+
+# 
