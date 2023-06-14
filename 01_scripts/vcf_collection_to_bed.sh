d2f7510a3706adbac3b84e5e3d782bab460bdd0a
GreenT <bensutherland7@gmail.com>
2023-06-14 12:31:22 -0700

    Remove message regarding 0-based, and improve commenting

--- 01_scripts/vcf_collection_to_bed.sh
+++ 01_scripts/vcf_collection_to_bed.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-# Uses various .csv files to create a bed file
+# Uses collected info from a VCF to create a bed file
 
 # Global variables
-OUTPUT_FOLDER="04_extract_loci"
+INPUT_FOLDER="10_designer"
+OUTPUT_FOLDER="10_designer"
 INPUT="vcf_selection.csv"
 OUTPUT="vcf_selection.bed"
 
 # Prepare bed file
-echo "From each position, will subtract 201 bp (start) and add 200 bp as moving from VCF position to bed (0-based) system"
+echo "From each position, will subtract 201 bp (start) and add 200 bp as moving from VCF position to bed"
 
 # Keep the chromosome (1), the lower range, upper range, and marker info
-awk -F, '{ print $1 "\t" $2-201 "\t" $2+200 "\t" "mname_"$3 }' $OUTPUT_FOLDER/$INPUT |
+awk -F, '{ print $1 "\t" $2-201 "\t" $2+200 "\t" "mname_"$3 }' $INPUT_FOLDER/$INPUT |
     
-    # Remove extra information in last field trailing the colon
+    # Remove any info after the first colon in the marker name column 
     awk -F: '{ print $1 }' - >  $OUTPUT_FOLDER/$OUTPUT
 
 
