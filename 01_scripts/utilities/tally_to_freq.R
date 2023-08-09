18b37ab348bc7d14d1e038671d0bb85ad582d9e9
GreenT <bensutherland7@gmail.com>
2023-08-08 17:08:44 -0700

    Update tally-to-freq script with new approach to not assume missing data

--- 01_scripts/utilities/tally_to_freq.R
+++ 01_scripts/utilities/tally_to_freq.R
@@ -4,48 +4,74 @@
 
 tally_to_freq <- function(df = "df", allele_source = "novel"){
   
+  # Replace lower case with the  capitalized term for later matching
+  allele_source <- gsub(pattern = "novel", replacement = "Novel", x = allele_source)
+  allele_source <- gsub(pattern = "hotspot", replacement = "Hotspot", x = allele_source)
+  
   # Reporting
   print(paste0("Assessing ", allele_source, " variants"))
   
-  # Set target of function
-  if(allele_source=="novel"){
-    
-    allele_source <- "Novel"
-    type          <- "Frequency of variant"
-    
-  }else if(allele_source=="hotspot"){
-    
-    allele_source <- "Hotspot"
-    type          <- "Minor allele frequency"
-    
-  }else(
-    
-    stop("The allele_source must be either novel or hotspot")
-    
-  )
-  
   # Provide warning
-  cat("Caution: \nfrequencies only approximated, \nassumes no missing data and \nabsence of calls as homo ref for novel variants")
-  
+  cat("Caution: \nnovel variant frequencies only approx. (absence of call is assumed reference) \nand AF is calculated based on amplicon's hotspot presence")
   head(df, n = 3)
-   
-  # Assess how many unique individuals are present in the df
-  num_indiv <- length(unique(x = df$identifier))
-  print(paste0("There are a total of ", num_indiv, " individuals present in the data"))
   
-  # Reporting
+  ## Assign locus names to all variants
   print("Assigning universal locus names (i.e., locus.id) to novel variants for broader comparison")
-  
-  # Assign locus names to all variants
   df$locus.id <- paste0(df$Chrom, "__", df$Position)
-  #print(head(df, n = 3))
+  head(df, n = 3)
   
-  # Reporting
+  ## How many unique individuals are present in the df?
+  print(paste0("There are a total of ", length(unique(x = df$identifier)), " individuals present in the data"))
+  
+  ## How many unique loci are present in the df?
   print(paste0("There are a total of ", length(unique(df$locus.id)), " unique loci"))
   
   # Retain backup before filtering
   df.bck <- df
 
+  #### 01. Num indiv genotyped at each amplicon ####
+  ## Determine how many individuals are genotyped at each region (i.e., amplicon)
+  # Retain only hotspot info
+  df_hotspot <- df[df$Allele.Source=="Hotspot", ]
+  head(df_hotspot)
+  nrow(df_hotspot)
+  # Remove any sample-locus that is a no call (i.e., missing)
+  df_hotspot <- df_hotspot[df_hotspot$Allele.Call!="No Call", ] # drop no calls
+  nrow(df_hotspot)
+  
+  # What are the unique locus IDs? Create a vector
+  total_locus.id <- unique(df_hotspot$locus.id)
+  length(total_locus.id)
+  
+  # Loop over the vector, and for each unique locus ID, determine how many samples were typed at that locus
+  # note: this may be replaced by summarise (later)
+  locus_of_interest <- NULL; num_indiv_genod_at_locus <- NULL; num_indiv_genod_at_locus.vec <- NULL
+  for(i in 1:length(total_locus.id)){
+    
+    locus_of_interest <- total_locus.id[i]
+    
+    num_indiv_genod_at_locus     <- length(df_hotspot[df_hotspot$locus.id==locus_of_interest, "Sample.Name"])
+    num_indiv_genod_at_locus     <- paste0(locus_of_interest, "___", num_indiv_genod_at_locus)
+    num_indiv_genod_at_locus.vec <- c(num_indiv_genod_at_locus.vec, num_indiv_genod_at_locus)
+    
+  }
+  
+  head(num_indiv_genod_at_locus.vec)
+  
+  # Make into df
+  num_indiv_genod_at_locus.df  <- as.data.frame(num_indiv_genod_at_locus.vec)
+  num_indiv_genod_at_locus.df  <- separate(data = num_indiv_genod_at_locus.df, col = "num_indiv_genod_at_locus.vec", into = c("locus.id", "indiv.genod")
+                                                    , sep = "___", remove = T
+                                          )
+  
+  num_indiv_genod_at_locus.df$indiv.genod <- as.numeric(num_indiv_genod_at_locus.df$indiv.genod)
+  num_indiv_genod_at_locus.df$total.poss.alleles <- num_indiv_genod_at_locus.df$indiv.genod * 2
+  head(num_indiv_genod_at_locus.df)
+  
+  merge(x = num_indiv_genod_at_locus.df, y = df, by = "locus.id")
+  
+  
+  #### 02. Tally variants ####
   # Only keep specified variants
   print(paste0("Retaining only specified variants from the ", tolower(allele_source), " allele source"))
   df <- df[df$Allele.Source==allele_source,]
@@ -60,7 +86,9 @@ tally_to_freq <- function(df = "df", allele_source = "novel"){
   df[df$Allele.Call=="Heterozygous", "allele.count"] <- 1
   df[df$Allele.Call=="Absent"|df$Allele.Call=="No Call", "allele.count"] <- 0
   
-  head(df, n = 3)
+  head(df, n = 3) 
+  
+  table(df$Allele.Call)
   
   # Tally minor allele (hotspot) or non-ref (novel) variants
   tallies <- df %>% group_by(locus.id) %>%
@@ -73,8 +101,41 @@ tally_to_freq <- function(df = "df", allele_source = "novel"){
   head(tallies)
   
   
+  #### Bring it all together ####
+  head(num_indiv_genod_at_locus.df) # this is actually the hotspot itself
+  head(tallies)
   
   
+  
+  # Obtain the Region.Name for each locus.id to attach to tallies
+  locus_and_regions <- df.bck[,c("locus.id", "Region.Name")]
+  head(locus_and_regions)
+  locus_and_regions <- paste0(locus_and_regions$locus.id, "___", locus_and_regions$Region.Name)
+  locus_and_regions <- unique(locus_and_regions)
+  locus_and_regions <- as.data.frame(locus_and_regions)
+  head(locus_and_regions)
+  locus_and_regions <- separate(data = locus_and_regions, col = "locus_and_regions", into = c("locus", "region"), sep = "___", remove = T)
+  head(locus_and_regions)
+  
+  test <- merge(x = tallies, y = locus_and_regions, by.x = "locus.id", by.y = "locus", all.x = T)
+  head(test)
+  nrow(test)
+  
+  # Now have the region, the locus id, and the total number of views
+  
+  
+  
+  # Combine with tallies
+  head(tallies)
+  nrow(tallies)
+  
+  all_data.df <- merge(x = tallies, y = num_indiv_genod_at_locus.df, by.x = "locus.id", by.y = "locus.id")
+  head(all_data.df)
+  nrow(all_data.df)
+  
+  #plot(all_data.df$total_views, all_data.df$total.poss.alleles)
+  
+  all_data.df$total_views / all_data.df$total.poss.alleles
   
   # Calculate frequency of , assuming 100% genotyping (no missing data)
   tallies$freq <- tallies$total_views / (num_indiv*2)
