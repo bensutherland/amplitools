c007dd46803a85a556287e867afb5fb765a4a4ab
GreenT <bensutherland7@gmail.com>
2023-08-16 12:03:13 -0700

    Clean up the demo analysis script

--- 01_scripts/demo_analysis.R
+++ 01_scripts/demo_analysis.R
@@ -1,31 +1,42 @@
-# First run 00_initiator.R
+## This is a demonstration analysis of VariantCaller input data to accompany the cgig_ms_panel manuscript
+# B. Sutherland (2023-08-16)
+# Please see the README for cgig_ms_panel for more detailed instructions
+
+#### 01. amplitools input to genepop format ####
+# Clear the workspace then source the 00_initiator.R to activate all functions
 
 # Convert all files in 02_input_data from proton to create genepop matrices
 proton_to_genepop(hotspot_only=TRUE, neg_control="BLANK")
-# Where neg_control is the name of the negative control wells in the study; and hotspot_only defines if only the hotspot targets would be used
+#  neg_control = name of the negative control wells
+#  hotspot_only = hotspot targets only (default and only method as of 2023-08-16)
 
 # Then run `amplitools/01_scripts/format_genepop.sh <filename>` for each file
 # You will then have *.gen files prepared in 02_input_data/prepped_matrices/
 
-# Next will be in SPS
+# Copy the .gen files into simple_pop_stats/02_input_data
 
-# Clear workspace and source simple_pop_stats_start.R
+
+#### 02. simple_pop_stats compare tech reps ####
+# Clear the workspace and source simple_pop_stats_start.R
+
 ## also source comp_tech_reps.R, which is currently in the dev scripts
 comp_tech_reps(format_type = "amplitools", max_missing = 0.5)
 
-save(obj_nr_best, file="02_input_data/obj_nr_best_2023-05-01.RData")
+date <- format(Sys.time(), "%Y-%m-%d")
+save(obj_nr_best, file=paste0("02_input_data/obj_nr_best_", date, ".RData"))
 
-# Restart before popgen analysis but after comparing tech reps? 
-# Clear workspace and source simple_pop_stats_start.R
-load("02_input_data/obj_nr_best_2023-05-01.RData")
 
-# Next use for all downstream popgen analyses
-obj_nr_best 
-pop(obj_nr_best) # note that all pop attributes are 'unkn' currently, they will be added in the script below
+#### 03. simple_pop_stats population genetic analysis
 
-# Complete all described in 'ms_amplicon_panel/01_scripts/sps_popgen_analysis.R'
-# This will output a rubias formatted file to 'amplitools/03_prepped_data/cgig_all_rubias.txt'
+# note: the current analysis uses the obj_nr_best object rather than a genepop as input
+#   , given the retention of the best tech rep
 
+## the population genetic analysis is all described in full in 
+#   'ms_amplicon_panel/01_scripts/sps_popgen_analysis.R' 
+#    and will output a rubias formatted file to 'amplitools/03_prepped_data/cgig_all_rubias.txt'
+
+
+#### 04. amplitools for parentage analysis ####
 # Source 00_initiator.R
 
 # Estimate log likelihoods from the data and simulated sibs/ parents, then calculate on your existing data
