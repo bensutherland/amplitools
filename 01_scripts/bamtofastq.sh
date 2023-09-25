36b3ca986ca015f7754f123ff01f01d79608311f
GreenT <bensutherland7@gmail.com>
2023-09-25 14:28:58 -0700

    Input bam to fq.gz initializing

---
+++ 01_scripts/bamtofastq.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Convert bam file to fastq file using bedtools
+
+# System variables
+INPUT_FOLDER="12_input_data_mhp"
+
+# Convert samples from BAM to FASTQ
+ls -1 $INPUT_FOLDER/*.bam | 
+    sort -u | 
+    while read i
+    do 
+        # Reporting
+        echo "Converting $i from bam to fq.gz"
+
+        # Convert from bam to fastq
+        bedtools bamtofastq -i $i -fq ${i%.bam}.fq
+
+        # Compress fq
+        gzip ${i%.bam}.fq
+
+    done 
+
