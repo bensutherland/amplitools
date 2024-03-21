27ca394bd34610f036452f8eadc09ec965c1785f
bensutherland <bensutherland7@gmail.com>
2024-03-21 14:44:04 -0700

    Add simple read counter to support picking top replicate prior to mhap calling

---
+++ 01_scripts/count_reads.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Run from main directory 
+# Assumes all fastq have four lines per record
+
+# User set variables
+INPUT_FOLDER="12_input_mhap"
+SUFFIX=".fastq"
+
+# Move into the directory containing files
+cd $INPUT_FOLDER 
+
+# Clear previous runs
+rm reads_per_sample.txt 2> /dev/null
+rm reads_per_sample_table.txt 2> /dev/null
+
+## Number Reads
+# Determine number of reads per file from samples file
+for i in $(ls *$SUFFIX) ; 
+    do echo $i ;
+    echo "$(cat $i | wc -l)" / 4 | bc ;
+    done >> reads_per_sample.txt
+
+# Separate by second line into two columns
+sed 'N;s/\n/ /' reads_per_sample.txt > reads_per_sample_table.txt
+
+# Remove intermediate file
+rm reads_per_sample.txt
+
