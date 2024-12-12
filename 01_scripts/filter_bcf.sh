e645386a336986a6e9c91170ad266850a6219467
bensutherland <bensutherland7@gmail.com>
2024-12-12 13:32:41 -0800

    Update filter defaults and add method to screen per genotype GQ score

--- 01_scripts/filter_bcf.sh
+++ 01_scripts/filter_bcf.sh
@@ -12,12 +12,13 @@ NAME=$(basename $0)
 NUM_CPU=36
 INPUT_BCF="mpileup_calls.bcf"
 INDEL_DIST=5      # indel surrounding window size around which to remove SNPs
-MISSING_PPN=0.1   # missing data proportion (default: filter if > 10%)
-QUAL=20           # minimum quality for SNP at least one indiv
+MISSING_PPN=0.15   # missing data proportion (default: filter if > 10%)
+QUAL=99           # minimum quality for SNP at least one indiv
 #MIN_TOTAL_DP=10   # minimum total depth for site
 MIN_AVG_DP=10     # minimum depth when averaged across all samples
-MIN_GENO_DP=4     # minimum geno depth or change to missing
-MAX_GENO_DP=100000   # maximum geno depth or change to missing
+MIN_GENO_DP=10     # minimum geno depth or change to missing
+MAX_GENO_DP=10000   # maximum geno depth or change to missing
+LOW_GQ_FILT=20
 
 # Copy script as it was run
 cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
@@ -67,7 +68,7 @@ bcftools view $DATAFOLDER/$OUTPUT_FN_3 --threads $NUM_CPU | grep -vE '^#' - | wc
 # 4. Filter based on quality
 echo "Keeping only variants with quality > $QUAL in at least one indiv" | tee -a $RESULT_LOG
 OUTPUT_FN_4="${OUTPUT_FN_3%.bcf}"_q$QUAL.bcf
-bcftools view -i "%QUAL>=$QUAL" --threads $NUM_CPU $DATAFOLDER/$OUTPUT_FN_3 \
+bcftools view -i "QUAL>=$QUAL" --threads $NUM_CPU $DATAFOLDER/$OUTPUT_FN_3 \
         -Ob -o $DATAFOLDER/$OUTPUT_FN_4
 echo "Completed, output as $DATAFOLDER/$OUTPUT_FN_4" | tee -a $RESULT_LOG
 
@@ -111,15 +112,26 @@ echo "Output BCF now has the following number of variants: " | tee -a $RESULT_LO
 bcftools view $DATAFOLDER/$OUTPUT_FN_7 --threads $NUM_CPU | grep -vE '^#' - | wc -l | tee -a $RESULT_LOG
 
 
-# 8. Filter again for missing data after the previous
-echo "Filtering out variants missing in more than $MISSING_PPN (proportion) individuals, following previous substitution" | tee -a $RESULT_LOG
-OUTPUT_FN_8="${OUTPUT_FN_7%.bcf}"_miss$MISSING_PPN.bcf
-bcftools view -i "F_missing < $MISSING_PPN" --threads $NUM_CPU $DATAFOLDER/$OUTPUT_FN_7 \
-        -Ob -o $DATAFOLDER/$OUTPUT_FN_8
+# 8. Remove low GQ genotypes
+echo "Set to missing low quality (GQ < $LOW_GQ_FILT) genotypes" | tee -a $RESULT_LOG
+OUTPUT_FN_8="${OUTPUT_FN_7%.bcf}"_minGQ"$LOW_GQ_FILT".bcf
+bcftools +setGT $DATAFOLDER/$OUTPUT_FN_7 -- -t q -n . -i "FMT/GQ<$LOW_GQ_FILT" > $DATAFOLDER/$OUTPUT_FN_8
 echo "Completed, output as $DATAFOLDER/$OUTPUT_FN_8" | tee -a $RESULT_LOG
 
 # Reporting
-echo "Output BCF now has the following number of variants: "
+echo "Output BCF now has the following number of variants: " | tee -a $RESULT_LOG
 bcftools view $DATAFOLDER/$OUTPUT_FN_8 --threads $NUM_CPU | grep -vE '^#' - | wc -l | tee -a $RESULT_LOG
+
+
+# 9. Filter again for missing data after the previous
+echo "Filtering out variants missing in more than $MISSING_PPN (proportion) individuals, following previous substitution" | tee -a $RESULT_LOG
+OUTPUT_FN_9="${OUTPUT_FN_8%.bcf}"_miss$MISSING_PPN.bcf
+bcftools view -i "F_missing < $MISSING_PPN" --threads $NUM_CPU $DATAFOLDER/$OUTPUT_FN_8 \
+        -Ob -o $DATAFOLDER/$OUTPUT_FN_9
+echo "Completed, output as $DATAFOLDER/$OUTPUT_FN_9" | tee -a $RESULT_LOG
+
+# Reporting
+echo "Output BCF now has the following number of variants: "
+bcftools view $DATAFOLDER/$OUTPUT_FN_9 --threads $NUM_CPU | grep -vE '^#' - | wc -l | tee -a $RESULT_LOG
 
 echo "Filtering complete" | tee -a $RESULT_LOG
